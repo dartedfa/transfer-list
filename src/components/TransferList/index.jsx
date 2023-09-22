ff48e19dbab32509befce73bc69748d9e54c661d
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2023-09-22 22:43:00 +0400

    Added scss

---
+++ src/components/TransferList/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function TransferList() {
+  return <div>Hey !</div>
+}
+
+export default TransferList
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
+import './sass/index.scss'
+import TransferList from 'components/TransferList'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <div></div>
+    <TransferList />
   </React.StrictMode>,
 )
 

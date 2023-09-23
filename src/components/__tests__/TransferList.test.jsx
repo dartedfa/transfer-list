026a399f8c7664c68faee59616db2397e850a0a6
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2023-09-23 19:18:15 +0400

    Added first test.

--- src/components/__tests__/TransferList.test.jsx
+++ src/components/__tests__/TransferList.test.jsx
@@ -1,15 +1,26 @@
 import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import TransferList from '../TransferList'
-import {generateRandomListItems} from '../../data'
+import {generateRandomListItems} from 'data'
 
 function renderTransferList(list = []) {
   return render(<TransferList list={list} />)
 }
 
+const moveAllToRightText = 'move all right'
+const moveSelectedToRightText = 'move selected right'
+const moveSelectedToLeftText = 'move selected left'
+const moveAllToLeftText = 'move all left'
+
 
 describe('TransferList', () => {
-  it('makes sure that move right button gets disabled after moving all items to right side', () => {
-    const list = generateRandomListItems()
-    const screen = renderTransferList(list)
+  it('makes sure that move right buttons gets disabled after moving all items to right side', () => {
+    const list = generateRandomListItems(2)
+    const {getByRole} = renderTransferList(list)
+
+    const moveAllToRightButton = getByRole('button', {name: moveAllToRightText})
+    expect(moveAllToRightButton).not.toBeDisabled()
+    userEvent.click(moveAllToRightButton)
+    expect(moveAllToRightButton).toBeDisabled()
   })
 })
